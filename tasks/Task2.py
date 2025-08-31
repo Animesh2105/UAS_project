234fc41f7947f581c28a9ba1d720fe2ee9e120bd
Animesh2105 <animesh06@hotmail.com>
2025-08-31 21:32:42 +0530

    Update Task2.py

--- tasks/Task2.py
+++ tasks/Task2.py
@@ -1,14 +1,29 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('E:/UAS/3.png')
+img = cv.imread('E:/UAS/2.png')
 grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 blurimg = cv.bilateralFilter(grayimg, 9, 75, 75)
 #Using Canny edge detection on a blurred and grayscale image
 edgeimg = cv.Canny(blurimg, 50, 150)
 
+#defining a function to the get the location of the casualties and the rescue pads
+def get_location(contour):
+    M = cv.moments(contour)
+    if M["m00"] == 0:  # avoids
+        return (0, 0)
+    cx = int(M["m10"] / M["m00"])
+    cy = int(M["m01"] / M["m00"])
+    return (cx, cy)
+
+
 #we use findContours to detect the external edges of the shapes in the image
 contours, hierarchies = cv.findContours(edgeimg, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+
+casualty_id = 1
+casualties = []
+pad_id = 1
+pads = []
 
 for contour in contours:
      area = cv.contourArea(contour)
@@ -16,15 +31,16 @@ for contour in contours:
         continue
      #this part was a bit tricky, approxPolyDp lets us approximate a set of contours as a polygon, needs an epsilon (an error value, basically)
      #some help from the internet was used to find what a good epsilon value is supposed to be
+     location = get_location(contour)
      perimeter = cv.arcLength(contour, True)
-     shape = cv.approxPolyDP(contour, 0.035*perimeter, True)
+     shape = cv.approxPolyDP(contour, 0.04*perimeter, True)
      edges = len(shape)
      if edges == 3:
-        age = "elderly"
+        age = 2   #elderly
      elif edges == 4:
-        age =  "adult"
+        age =  1  #adult
      elif edges != 8: #this is a bit of frankencode, cant get approxpolydp to always recognise stars without breaking everything else and circles come up as 8 edges so..
-        age = "children"
+        age = 3   #children
      else:
         continue
      #now that we have differentiated the shape of the objects, we move to segmenting them by colour
@@ -33,12 +49,21 @@ for contour in contours:
      B, G, R, alpha = cv.mean(img, mask = mask)    #this returns the mean bgr values of that specific shape we mask
      #now we use the bgr values to assign emergency levels to the victims, and designated capacities to the rescue pads
      if edges != 8:
-        if R > 150 and G > 150 and B < 100:
-            emergency = "moderate"
-        elif R > 150 and G < 100 and B < 100:
-            emergency = "severe"
-        elif G > 150 and R < 100 and B < 100:
-            emergency = "safe"
+      if R > 120 and G > 120 and B < 120 and abs(R - G) < 60:
+         emergency = 2   #moderate
+      elif R > 150 and G < 120 and B < 120:
+         emergency = 3   # severe
+      elif G > 150 and R < 120 and B < 120:
+         emergency = 1   # safe
+      else:
+         emergency = 0   
+      casualties.append({
+           "casualty id": casualty_id, 
+           "age": age, 
+           "emergency": emergency,
+           "location": location
+        })
+      casualty_id=casualty_id+1
      elif edges == 8:   #since circles show up as 8 edges
           if R > 180 and B > 180 and G < 150:
             capacity = 3
@@ -46,14 +71,9 @@ for contour in contours:
             capacity = 4
           elif abs(R - G) < 20 and abs(G - B) < 20 and 80 < R < 200:
             capacity = 2
+     print(edges)
+     cv.imshow('mask', mask)
+     cv.imshow('img', img)
+     cv.waitKey(0)
+print(casualties)
       
-
-
-        
-     
-
-
-
-        
-     
-
